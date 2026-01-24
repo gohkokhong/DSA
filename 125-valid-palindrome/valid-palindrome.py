5263b29b1e20ec7ecb9f016f459d35f740c851ea
Goh Kok Hong <30322984+gohkokhong@users.noreply.github.com>
2026-01-24 22:32:23 +0800

    Time: 7 ms (82.79%) | Memory: 19.6 MB (43.29%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.py
+++ 125-valid-palindrome/valid-palindrome.py
@@ -1,14 +1,24 @@
 class Solution:
-    def isPalindrome(self, s: str) -> bool:        
-        # Initialise a new string
-        cleanedStr = ''
+    def isPalindrome(self, s: str) -> bool:
+        # Initialise two pointers
+        left, right = 0, len(s)-1
 
-        # Iterate through the string
-        for char in s:
-            # If the char isalnum:
-            if char.isalnum():
-                # Add into the string as lower case
-                cleanedStr += char.lower()
+        # While left is less than r:
+        while left < right:
+            # Move left forward until it points to an alphanumeric char
+            while left < right and not s[left].isalnum():
+                left += 1
+            # Move right backward until it points to an alphanumeric char
+            while right > left and not s[right].isalnum():
+                right -= 1
 
-        # Return string and reversed
-        return cleanedStr == cleanedStr[::-1]
+            # Compare the lower case chars at left and right
+            if s[left].lower() != s[right].lower():
+                # If they don't match, return False
+                return False
+
+            # Move both pointers inward l += 1, r -= 1
+            left, right = left + 1, right - 1
+
+        # If loop finishes without mismatch, return True
+        return True
