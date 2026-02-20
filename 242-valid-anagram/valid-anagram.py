fda564f52815bb7ccfc649d05820d1fb26711cb7
Goh Kok Hong <30322984+gohkokhong@users.noreply.github.com>
2026-02-20 19:37:48 +0800

    Time: 7 ms (90.76%) | Memory: 19.4 MB (71.69%) - LeetSync

--- 242-valid-anagram/valid-anagram.py
+++ 242-valid-anagram/valid-anagram.py
@@ -1,15 +1,15 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
+        # Strings have length mismatch
         if len(s) != len(t):
             return False
 
-        sDict = {}
-        tDict = {}
+        # Initialise dictionaries
+        sDict, tDict = {}, {}
 
         for index in range(len(s)):
             sDict[s[index]] = 1 + sDict.get(s[index], 0)
             tDict[t[index]] = 1 + tDict.get(t[index], 0)
-
 
         if sDict == tDict:
             return True
